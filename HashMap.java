d4b145c5dcbc81b20e6cd661888e689121845932
sai <23b81a728@cvr.ac.in>
2025-04-21 15:02:32 +0530

    hello

---
+++ HashMap.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class HashMap{
+	public static void main(String[] args){
+		HashMap<String,Integers> hmc1=new HashMap<>();
+		hmc1.put("hi",113);
+		hmc1.put("hello",123);
+		hmc1.put("namasthey",143);
+		hmc1.put("hola",153);
+		hmc1.put(163);
+		hmc1.put(173);
+		System.out.println("mappings of HashMap hmc1 are:"+hmc1);
+		HashMap<Char,Integer> hmc2=new HashMap<Char,Integer>();
+		hmc2.put("a",1);
+		hmc2.put("b",2);
+		hmc2.put("c",3);
+		hmc2.put("d",4);
+		hmc2.put("e",5);
+		System.out.println("mappings of HashMap hmc2 are:"+hmc2);
+	}
+}
